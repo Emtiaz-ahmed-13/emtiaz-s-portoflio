8af5bacfb9172cb87ca975491209072d5696def8
Emtiaz-ahmed-13 <emtiaz.ahmed@g.bracu.ac.bd>
2024-10-21 14:10:02 +0600

    finish almost

--- data/index.ts
+++ data/index.ts
@@ -72,35 +72,27 @@ export const gridItems = [
 export const projects = [
   {
     id: 1,
-    title: "3D Solar System Planets to Explore",
-    des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
-    img: "/p1.svg",
-    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
-    link: "/ui.earth.com",
+    title: "Paste",
+    des: "Paste is a day to day daily task manager. It keeps you organised and on top of your day.",
+    img: "https://i.ibb.co.com/9b9dxX5/Screenshot-2024-10-21-at-1-16-00-PM.png",
+    iconLists: ["/re.svg", "/tail.svg"],
+    link: "https://paste-r2gqo2v23-emtiaz-ahmed-13s-projects.vercel.app/",
   },
   {
     id: 2,
-    title: "Yoom - Video Conferencing App",
-    des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
-    img: "/p2.svg",
-    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
-    link: "/ui.yoom.com",
+    title: "Ts Store",
+    des: "Advanced Filtering & Sorting by price, rating, and more Responsive Grid Layout showcasing product title, price, and ratingSearch & Pagination for smooth navigation",
+    img: "https://i.ibb.co.com/QQ0T4vN/Screenshot-2024-10-21-at-1-35-56-PM.png",
+    iconLists: ["/ts.svg", "/tail.svg"],
+    link: "https://ts-store-ibvd.vercel.app/",
   },
   {
     id: 3,
-    title: "AI Image SaaS - Canva Application",
-    des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
-    img: "/p3.svg",
-    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
-    link: "/ui.aiimg.com",
-  },
-  {
-    id: 4,
-    title: "Animated Apple Iphone 3D Website",
-    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
-    img: "/p4.svg",
-    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
-    link: "/ui.apple.com",
+    title: "Music Album",
+    des: "MY New Fun Project For - Exploring : Aceternity ui  NextJs (basic)  TypeScript(basic) .",
+    img: "https://i.ibb.co.com/fQz7v8k/Screenshot-2024-10-21-at-1-45-47-PM.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg"],
+    link: "https://66c03ad8f0169f11c9266088--sweets-music.netlify.app/",
   },
 ];
 
@@ -140,64 +132,22 @@ export const testimonials = [
 export const companies = [
   {
     id: 1,
-    name: "cloudinary",
-    img: "/cloud.svg",
-    nameImg: "/cloudName.svg",
-  },
-  {
-    id: 2,
     name: "appwrite",
     img: "/app.svg",
     nameImg: "/appName.svg",
   },
   {
+    id: 2,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+
+  {
     id: 3,
-    name: "HOSTINGER",
-    img: "/host.svg",
-    nameImg: "/hostName.svg",
-  },
-  {
-    id: 4,
-    name: "stream",
-    img: "/s.svg",
-    nameImg: "/streamName.svg",
-  },
-  {
-    id: 5,
     name: "docker.",
     img: "/dock.svg",
     nameImg: "/dockerName.svg",
-  },
-];
-
-export const workExperience = [
-  {
-    id: 1,
-    title: "Frontend Engineer Intern",
-    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
-    className: "md:col-span-2",
-    thumbnail: "/exp1.svg",
-  },
-  {
-    id: 2,
-    title: "Mobile App Dev - JSM Tech",
-    desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
-    className: "md:col-span-2", // change to md:col-span-2
-    thumbnail: "/exp2.svg",
-  },
-  {
-    id: 3,
-    title: "Freelance App Dev Project",
-    desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
-    className: "md:col-span-2", // change to md:col-span-2
-    thumbnail: "/exp3.svg",
-  },
-  {
-    id: 4,
-    title: "Lead Frontend Developer",
-    desc: "Developed and maintained user-facing features using modern frontend technologies.",
-    className: "md:col-span-2",
-    thumbnail: "/exp4.svg",
   },
 ];
 
--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -6,9 +6,9 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://7b883d075caa19b41fd9b00ae313a1c6@o4506813739368448.ingest.us.sentry.io/4507222371729408",
+  dsn: "https://d7bebb6fa330671904be953b4bff5a37@o4508159667535872.ingest.us.sentry.io/4508159669305344",
 
-  // Adjust this value in production, or use tracesSampler for greater control
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
